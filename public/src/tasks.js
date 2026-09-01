c2497da4d347ecc769c4bc7108456e22ce990626
Keckpy <36435107+Keckpy@users.noreply.github.com>
2026-08-31 23:53:42 -0400

    Add click listeners for buttons on tasks page

--- public/src/tasks.js
+++ public/src/tasks.js
@@ -8,6 +8,77 @@ const urinesForm = document.getElementById('urines-bench');
 const ancillaryForm = document.getElementById('ancillary');
 const remisolForm = document.getElementById('remisol');
 
+centaurBtn.addEventListener('click', (e) => {
+    centaurForm.classList.toggle('hidden');
+    e.preventDefault();
+    
+    if (!ancillaryForm.classList.contains('hidden')) {
+        ancillaryForm.classList.toggle('hidden');
+        e.preventDefault();
+    }
+    if (!urinesForm.classList.contains('hidden')) {
+        urinesForm.classList.toggle('hidden');
+        e.preventDefault();
+    }
+    if (!remisolForm.classList.contains('hidden')) {
+        remisolForm.classList.toggle('hidden');
+        e.preventDefault();
+    }
+})
+
+urinesBtn.addEventListener('click', (e) => {
+    urinesForm.classList.toggle('hidden');
+    e.preventDefault();
+
+    if (!ancillaryForm.classList.contains('hidden')) {
+        ancillaryForm.classList.toggle('hidden');
+        e.preventDefault();
+    }
+    if (!centaurForm.classList.contains('hidden')) {
+        centaurForm.classList.toggle('hidden');
+        e.preventDefault();
+    }
+    if (!remisolForm.classList.contains('hidden')) {
+        remisolForm.classList.toggle('hidden');
+        e.preventDefault();
+    }
+})
+
+ancillaryBtn.addEventListener('click', (e) => {
+    ancillaryForm.classList.toggle('hidden');
+    e.preventDefault();
+
+    if (!urinesForm.classList.contains('hidden')) {
+        urinesForm.classList.toggle('hidden');
+        e.preventDefault();
+    }
+    if (!centaurForm.classList.contains('hidden')) {
+        centaurForm.classList.toggle('hidden');
+        e.preventDefault();
+    }
+    if (!remisolForm.classList.contains('hidden')) {
+        remisolForm.classList.toggle('hidden');
+        e.preventDefault();
+    }
+})
+
+remisolBtn.addEventListener('click', (e) => {
+    remisolForm.classList.toggle('hidden');
+    e.preventDefault();
+
+    if (!urinesForm.classList.contains('hidden')) {
+        urinesForm.classList.toggle('hidden');
+        e.preventDefault();
+    }
+    if (!centaurForm.classList.contains('hidden')) {
+        centaurForm.classList.toggle('hidden');
+        e.preventDefault();
+    }
+    if (!ancillaryForm.classList.contains('hidden')) {
+        ancillaryForm.classList.toggle('hidden');
+        e.preventDefault();
+    }
+})
 
 document.addEventListener('keydown', (e) => {
     // Press 'b' when on page
