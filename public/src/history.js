3bf29e30b607a8ae3c4f7df5a05298345d45039e
Keckpy <36435107+Keckpy@users.noreply.github.com>
2026-09-13 17:24:30 +0000

    Remove preventdefault on history page

--- public/src/history.js
+++ public/src/history.js
@@ -1,7 +1,6 @@
 const historyBtn = document.querySelector('.historyBtn');
 
 document.addEventListener('keydown', (e) => {
-    e.preventDefault();
     if (e.key === 'b') {
         window.location.href = '/notes';
     }
